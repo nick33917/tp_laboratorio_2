70ba9e5ae6192cfe488680cfe2437baae5459044
nick33917 <nick33917@hotmail.com>
2018-04-17 23:18:28 -0300

    faltan algunas validaciones

---
+++ Entidades/Entidades/Numero.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        private double ValidarNumero(string srtNumero)
+        {
+            
+            double num=0;
+            bool flag;
+            flag= double.TryParse(srtNumero,out num);
+            return num;
+        }
+
+        public string SetNumero 
+        { 
+             set {this.numero = this.ValidarNumero(value);}
+        }
+               
+        public static string BinarioDecimal(string binario)
+        {
+           string numDecimal = Convert.ToInt64(binario,2).ToString();
+           return numDecimal;
+
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            string numBinario = Convert.ToString((long)numero,2);
+            return numBinario;
+        }
+       
+        public Numero () : this(0)
+        {
+        }
+       
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string strNumero)
+        {
+            this.SetNumero = strNumero;
+           
+        }
+
+        public static double operator -(Numero n1, Numero n2)
+        {
+            double rtn;
+            rtn = n1.numero - n2.numero;
+            return rtn;
+        }
+        public static double operator +(Numero n1, Numero n2)
+        {
+            double rtn;
+            rtn = n1.numero + n2.numero;
+            return rtn;
+        }
+        public static double operator *(Numero n1, Numero n2)
+        {
+            double rtn;
+            rtn = n1.numero * n2.numero;
+            return rtn;
+        }
+
+        public static double operator /(Numero n1, Numero n2)
+        {
+            double rtn=0;
+            if (n2.numero != 0)
+            {
+                rtn = n1.numero / n2.numero;
+            } 
+            return rtn;
+            
+
+        }
+
+
+
+    }
+}
---
+++ Entidades/MiCalculadora/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class LaCalculadora : Form
+    {
+        public LaCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+            this.lblResultado.Text ="" ;
+            this.cmbOperador.Text ="" ;
+            
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private static double Operar(string numero1,string numero2,string operador)
+        {
+            double resultado;
+            Calculadora calcu = new Calculadora();
+            Numero n1= new Numero(numero1);
+            Numero n2 = new Numero(numero2);
+
+            resultado = calcu.Operar(n1,n2,operador);
+            return resultado;
+
+        }
+        private  void btnOperar_Click(object sender, EventArgs e)
+        {
+            string num1 = txtNumero1.Text;
+            string num2 = txtNumero2.Text;
+            string operador = cmbOperador.Text;
+            double resultado = LaCalculadora.Operar(num1, num2, operador);
+            lblResultado.Visible = true;
+            lblResultado.Text = resultado.ToString();
+
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+             string resultado = Numero.DecimalBinario(Convert.ToDouble(lblResultado.Text));
+             lblResultado.Text = resultado;
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+              string resultado = Numero.BinarioDecimal(lblResultado.Text);
+              lblResultado.Text = resultado;
+         }
+
+        private void lblResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
